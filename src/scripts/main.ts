79f0a99d6b7b1cd8a74c546bf6a86bf54dfa5234
BenGaudry <totoro42360@gmail.com>
2023-01-29 15:17:13 +0100

    Added comments in script

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,9 @@
 export {};
 
+// Hover effect on menu
 let navLinks = document.getElementsByClassName("tb-nav-link");
 for (let i = 0; i < navLinks.length; i++) {
+  // When hovering a link, the other links opacity is reduced
   navLinks[i].addEventListener("mouseenter", function () {
     for (let x = 0; x <= navLinks.length; x++) {
       if (x !== i) {
@@ -9,6 +11,7 @@ for (let i = 0; i < navLinks.length; i++) {
       }
     }
   });
+  // Reset the opacity on the links when the link isn't hovered anymore
   navLinks[i].addEventListener("mouseleave", function () {
     for (let x = 0; x <= navLinks.length; x++) {
       navLinks[x].classList.remove('tb-transp-nav-link')
@@ -16,6 +19,7 @@ for (let i = 0; i < navLinks.length; i++) {
   })
 }
 
+// Set a background on the menu when scrolling to upgrade readability
 window.onscroll = function() {
   console.log(document.documentElement.scrollTop)
   if (document.documentElement.scrollTop > 0) {
