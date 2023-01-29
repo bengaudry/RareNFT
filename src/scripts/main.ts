a492e2c5ab54cc69b50c8e6f7abebc14b04baea6
BenGaudry <totoro42360@gmail.com>
2023-01-29 16:35:26 +0100

    Optimization in scripts

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,11 +1,12 @@
 export {};
 
 // Hover effect on menu
-let navLinks = document.getElementsByClassName("tb-nav-link");
-for (let i = 0; i < navLinks.length; i++) {
+let navLinks = document.querySelectorAll(".tb-nav-link");
+const len = navLinks.length;
+for (let i = 0; i < len; i++) {
   // When hovering a link, the other links opacity is reduced
   navLinks[i].addEventListener("mouseenter", function () {
-    for (let x = 0; x < navLinks.length; x++) {
+    for (let x = 0; x < len; x++) {
       if (x !== i) {
         navLinks[x].classList.add("tb-transp-nav-link");
       }
@@ -13,19 +14,27 @@ for (let i = 0; i < navLinks.length; i++) {
   });
   // Reset the opacity on the links when the link isn't hovered anymore
   navLinks[i].addEventListener("mouseleave", function () {
-    for (let x = 0; x < navLinks.length; x++) {
+    for (let x = 0; x < len; x++) {
       navLinks[x].classList.remove("tb-transp-nav-link");
     }
   });
 }
 
-// Set a background on the menu when scrolling to upgrade readability
+let ticking = false;
 function setBackgroundOnMenu() {
-  if (document.documentElement.scrollTop > 0) {
-    document.querySelector(".top-bar")?.classList.add("scrolling-top-bar");
-  } else {
-    document.querySelector(".top-bar")?.classList.remove("scrolling-top-bar");
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      if (document.documentElement.scrollTop > 0) {
+        document.querySelector(".top-bar")?.classList.add("scrolling-top-bar");
+      } else {
+        document
+          .querySelector(".top-bar")
+          ?.classList.remove("scrolling-top-bar");
+      }
+      ticking = false;
+    });
   }
+  ticking = true;
 }
-window.addEventListener("DOMContentLoaded", () => setBackgroundOnMenu());
-window.addEventListener("scroll", () => setBackgroundOnMenu());
+
+window.addEventListener("scroll", setBackgroundOnMenu, { passive: true });
